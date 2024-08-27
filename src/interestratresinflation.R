b08c902a70e704a2216b3856f71038b2049b9cff
Garett Kaube <108091263+GarettKaube@users.noreply.github.com>
2024-08-27 09:42:56 -0600

    Update interestratresinflation.R

--- src/interestratresinflation.R
+++ src/interestratresinflation.R
@@ -16,68 +16,37 @@ library(MASS)
 library(robustbase) 
 library(olsrr)
 library(readtext)
-library(readtext)
+library(ggcorrplot)
 
 source("src/helpers.R")
 source("src/data.R")
-
-# to do:
-# get firm inventories
 
 
 code = readLines("./FRED/fred-api-key.txt")
 fredr_set_key(code)
 
-# Get data and create covid dummy variable
 data = data_pipeline()
-data$COVID = data[, 'date'] %>% sapply(FUN = covid_dummy)
+row.names(data) = 1:dim(data)[1]
 dim(data)
+tail(data)
+head(data,1)
+data$anticipated = as.numeric(data$anticipated)
+# Note: SP500_DIFF is in log differences, i.e log returns
 
-# Note: SP500_DIFF is in log differences
-
-data.ts = ts(dplyr::select(data, -c("date")), start = c(2015, 10), freq=12)
-data.ts = window(data.ts, start = c(2015,08))
+data.ts = ts(dplyr::select(data, -c("date")), start = c(2002, data[1, "month"]), freq=12)
+data.ts = window(data.ts, end = c(2019,1))
 dim(data.ts)
 
-# Plot inflation rate vs change in real interest rates
-autoplot(data.ts[, c("inflation", "REAL_INTEREST_3M_DIFF")], facets = TRUE)
 
-autoplot(data.ts[, c("NONE_FARM_PAYROLLS_DIFF")])
-
-# Plot inflation rate vs change in  interest rates
-autoplot(data.ts[, c("inflation", "TREASURY3M_DIFF")], facets = TRUE)
-
-# We see positive correlation with inflation and yields as an increase in inflation will cause investors to 
-# require higher yield to compensate for inflation.
-
-# Plot time series of some of the variables
-autoplot(data.ts[, "average_log_return"])
-autoplot(diff(log(data.ts[, "Stock_Index"])))
-autoplot(diff(log(data.ts[, "OIL_WTI"])))
-autoplot(data.ts[, "REAL_INTEREST_1Y_DIFF"])
-autoplot(data.ts[, "CONSUMPTION_DIFF"])
-autoplot(data.ts[, "FEDFUNDS_DIFF"])
-
-
-# Look at linear correlation
-cor_matrix = data.ts %>% cor()
 
 # Look at cross correlations
-print(ccf(data$TREASURY1Y_DIFF, data$inflation, lag.max= 20))
-print(ccf(data$TREASURY3M_DIFF, data$inflation), lag.max= 20)
+print(ccf(data$TREASURY1Y_DIFF, data$log_inflation, lag.max= 20))
+print(ccf(data$TREASURY3M_DIFF, data$log_inflation), lag.max= 20)
 print(ccf(data$TREASURY3M_DIFF, data$SP500_DIFF, lag.max= 20))
 
 
 
-# Histograms
-ggplot(data=data, aes(x=TREASURY3M_DIFF)) +
-  geom_histogram()
-ggplot(data=data, aes(x=Stock_Index_DIFF)) +
-  geom_histogram()
-ggplotly()
-
-
-
+# Assess multicollinearity 
 get_VIF = function(predictors) {
   # Initialize VIF data frame
   VIF = data.frame()
@@ -105,94 +74,166 @@ get_VIF = function(predictors) {
   return(VIF)
 }
 
-predictors = c("CONSUMPTION_DIFF1", "log_inflation1", "SAVINGS_DIFF1", "FEDFUNDS_DIFF",
-               "INFLATION_EXPECTATIONS_5Y_DIFF", "UNEMPLOYMENT_DIFF1", "NONE_FARM_PAYROLLS1", "producer_price_index_change1")
+predictors = c("CONSUMPTION_DIFF", "log_inflation", "SAVINGS_DIFF", "FEDFUNDS_DIFF"
+               ,"UNEMPLOYMENT_DIFF", "NONE_FARM_PAYROLLS", "VOLATILITY_INDEX_DIFF",
+               "FEDFUNDS_FUTURES_DIFF", "NOMINAL_GDP_DIFF")
 get_VIF(predictors)
 
-# Linear models
+# Look at correlation between the variables
+cor(data.ts[,c("UNEMPLOYMENT_DIFF", "CONSUMPTION_DIFF")])
+cor(data.ts[,c("SP500_DIFF", "CONSUMPTION_DIFF")])
+cor(data.ts[,c("SP500_DIFF", "OIL_WTI_DIFF")])
+cor(data.ts[,c("log_inflation", "OIL_WTI_DIFF")])
+cor(data.ts[,c("CONSUMPTION_DIFF", "NOMINAL_GDP_DIFF")])
 
-# notice that the correlation between consumption and unemployment is high:
-cor(data[,c("UNEMPLOYMENT_DIFF", "CONSUMPTION_DIFF")])
-cor(data[,c("INFLATION_EXPECTATIONS_5Y_DIFF", "CONSUMPTION_DIFF")])
-cor(data[,c("SP500_DIFF", "CONSUMPTION_DIFF")])
-cor(data[,c("SP500_DIFF", "CONSUMPTION_DIFF", "FEDFUNDS_DIFF1", "log_inflation", "log_inflation1", "NONE_FARM_PAYROLLS1", "producer_price_index_change1")])
+vars = c("SP500_DIFF", "CONSUMPTION_DIFF", "FEDFUNDS_DIFF", 
+         "log_inflation", "NONE_FARM_PAYROLLS_DIFF", 
+         "VOLATILITY_INDEX_DIFF", "OIL_WTI_DIFF", "NOMINAL_GDP_DIFF", 
+         "FEDFUNDS_FUTURES_DIFF" ,"inflation")
+
+corr = cor(data.ts[, vars])
+ggcorrplot(corr,hc.order = TRUE, outline.col = "white")
+
+# notice that the correlation between consumption_diff and unemployment_diff is high
 # so we will include only CONSUMPTION_DIFF in the model
 
-# We control with using consumption growth, and inflation as a measure of economic performance
-# This model would not satisfy strict exogeneity as a change in SP500 returns should not effect the FED RATE but SP500 may 
-# respond to future forecasts of inflation and hence a change in FED RATE.
-
-# We lag all the control variables since the stock market reacts to economic data from the previous period  
-
+#-----------------------------------------------------------------------
+#------------------------------  Modeling ------------------------------
+#-----------------------------------------------------------------------
+# Naive models
 model = dynlm(
-  SP500_DIFF ~  d(L(FEDFUNDS_DIFF, 0:4)) + L(FEDFUNDS_DIFF, 5) + 
-    L(log_inflation_MOM) + L(CONSUMPTION_DIFF) + COVID + 
-    L(producer_price_index_change) + L(NONE_FARM_PAYROLLS), 
+  SP500_DIFF ~ FEDFUNDS_DIFF, 
   data=data.ts
 )
 
+# HAC standard errors for testing model coefficients
+m = ceiling(0.75 * (dim(data.ts)[1])**(1/3))
+nw = NeweyWest(model, lag = m, prewhite = F)
+coeftest(model, vcov. = nw)
+
+# Distributed lag model with no controls
+model = dynlm(
+  SP500_DIFF ~  d(L(FEDFUNDS_DIFF, 0:4)) + L(FEDFUNDS_DIFF, 5), 
+  data=data.ts
+)
+# HAC standard errors for testing model coefficients
+m = ceiling(0.75 * (dim(data.ts)[1])**(1/3))
+nw = NeweyWest(model, lag = m, prewhite = F)
+coeftest(model, vcov. = nw)
+
+#------ Dynamic Linear Model---------
+# Controls:
+# - Present and past consumption growth, inflation, and savings growth: 
+#    Present values will directly effect firms and consumers 
+#    and investors react to the months previous values.
+# - Volatility index:
+#    Captures investors market sentiment.
+# - Anticipated rate changes (anticipated):
+#    A change in the FED rate may have no effect on returns if the change was anticipated
+
+# The dynamic linear model model would not satisfy strict exogeneity meaning that a change in SP500 returns may indirectly 
+# effect future federal funds rates through increased consumption.
+# but SP500 may respond to future forecasts of inflation and a change in FED EFFECTIVE RATE.
+
+model = dynlm(
+  SP500_DIFF ~  L(FEDFUNDS_DIFF, 0:10) + 
+    L(log_inflation) + log_inflation + VOLATILITY_INDEX_DIFF + 
+    anticipated + L(CONSUMPTION_DIFF, 0:1) + L(SAVINGS_DIFF, 0:1), 
+  data=data.ts
+)
+summary(model)
 
 # HAC standard errors
 m = ceiling(0.75 * (dim(data.ts)[1])**(1/3))
 nw = NeweyWest(model, lag = m, prewhite = F)
 coeftest(model, vcov. = nw)
 
+# Plot the coefficients
+lags=10
+coef(model)
+coef_df = data.frame(coef(model)[2:(lags+2)])
+coef_df=rownames_to_column(coef_df, var="coefficient")
+ggplot(data = coef_df, aes(x=1:(lags+1), y=coef.model..2..lags...2.., group=1)) +
+  geom_line(color="Dodgerblue")+
+  theme(axis.text.x = element_text(angle = 65, hjust = 1)) +
+  ylab("Coefficient") +
+  xlab("Lag") + 
+  ggtitle("Cumulative Dynamic Effect of Fed Effective Rate on S&P500") +
+  theme_minimal()
+
+# We see that a increase in the FED EFFECTIVE Rate causes the SP500 to decline
+# and quickly recovers
+
+# Look at autocorrelation and partial autocorrelation
 ggAcf(residuals(model))
 ggPacf(residuals(model))
 
-# We add log inflation, consumption_diff, savings as a measure of economic performance as inflation will be higher when consumption is high. 
-# i.e high consumption growth causing output to be larger than potential output may cause inflation.
-# Also note that in a market clearing environment Y(t) = C(t) in theory.
-# REAL_INTEREST_3M_DIFF also must become statistically insignificant because inflation will already be large when interest rates decide 
-# to increase in the same month so inflation absorbs the effect.
-# Inflation may not always move the same direction as the S&P500 as if we have a negative supply shock, output will decrease,
-# inflation will rise, and weak economic conditions may cause the S&P500 to fall due to weak consumer demand and investment.
-
-residual_plot("FEDFUNDS_DIFF", model, data=data.ts,lags=8)
-residual_plot("inflation", model, data=data.ts,lags=6)
-residual_plot("CONSUMPTION_DIFF", model, data=data.ts,lags=6)
-
+# Assess residuals
+residual_plot("FEDFUNDS_DIFF", model, data=data.ts,lags=lags)
+ggplotly()
+residual_plot("log_inflation", model, data=data.ts,lags=lags)
+residual_plot("CONSUMPTION_DIFF", model, data=data.ts,lags=lags)
+residual_plot("SAVINGS_DIFF", model, data=data.ts,lags=lags)
+residual_plot("VOLATILITY_INDEX_DIFF", model, data=data.ts,lags=lags)
 
 # F-test with HAC errors
-res_model <- dynlm(SP500_DIFF ~ 1, data=data[7:dim(data)[1], ])
+
+ANOVA_DATA = data.ts[(lags+1) : dim(data.ts)[1]-1, ]
+row.names(ANOVA_DATA) = 1:dim(ANOVA_DATA)[1]
+ANOVA_DATA = ts(ANOVA_DATA)
+
+res_model <- dynlm(SP500_DIFF ~ 1, data=ANOVA_DATA)
 waldtest(model, 
          res_model, 
          vcov = NeweyWest(model, lag = m, prewhite = F))
+# We Reject H0: all betas are 0.
 
 
-res_model <- update(model, formula = . ~ . - CONSUMPTION_DIFF - inflation)
-waldtest(model, 
-         res_model, 
-         vcov = NeweyWest(model, lag = m, prewhite = F))
-
-# Test another model
-model = dynlm(SP500_DIFF ~ REAL_INTEREST_3M_DIFF + d(L(REAL_INTEREST_3M_DIFF, 1:6)) + COVID + CONSUMPTION_DIFF, data=data.ts)
-summary(model)
-# HAC consistent standard errors
-m = ceiling(0.75 * (dim(data.ts)[1])**(1/3))
-nw = NeweyWest(model, lag = m, prewhite = F)
-coeftest(model, vcov. = nw)
-
-#VECTOR AUTOREGRESSIONS
+##-------VECTOR AUTOREGRESSION---------
+# Now we test if SP500 returns are endogenous. 
+# Macroeconomic models can contain a part in the consumer problem
+# Where consumers can decide to invest in stocks and bonds 
+# and therefore in the next period, the returns on the stocks/bonds
+# can effect consumer demand then consumer demand effects inflation and hence indirectly
+# the Fed Funds Rate.
 library(vars)
 
 interest_rate = "FEDFUNDS_DIFF"
+inflation_measure = "log_inflation"
 
-TYPE = 'none'
+# Intercept for VAR
+TYPE = 'const'
 
-var_data = data.ts[, c("log_inflation_MOM", "UNEMPLOYMENT_DIFF", interest_rate)]
-var_data = window(var_data, start = c(2020, 9))
-print(ccf(var_data[, "log_inflation"], var_data[, interest_rate], lag.max=10))
+var_data = data.ts[, c(inflation_measure, 
+                       "CONSUMPTION_DIFF", 
+                       "UNEMPLOYMENT_DIFF", 
+                       interest_rate, "SP500_DIFF")
+                    ]
+print(ccf(var_data[, inflation_measure], var_data[, interest_rate], lag.max=10))
+
 autoplot(var_data, facets = TRUE, colour=TRUE)
-VARselect(var_data, lag.max = 6,type=TYPE)[["selection"]]
+VARselect(
+  var_data, 
+  lag.max=6, 
+  type=TYPE, 
+  exogen = as.data.frame(data.ts[,"anticipated"])
+)[["selection"]]
 
-var.model = VAR(var_data, p=1, type=TYPE)
+var.model = VAR(
+  var_data, 
+  p=2, 
+  type=TYPE, 
+  exogen = 
+    as.data.frame(data.ts[,"anticipated"])
+)
 serial.test(var.model, lags.pt = 10, type="PT.asymptotic")
 summary(var.model)
+# x is our anticipated rate change dummy
 
 # TREASURY1Y_DIFF Granger-cause's inflation but not the other way around
-causality(var.model, "log_inflation_MOM")$Granger
+causality(var.model, "log_inflation")$Granger
 causality(var.model, interest_rate)$Granger
+grangertest(SP500_DIFF ~ FEDFUNDS_DIFF, order=2, data = var_data)
 
 # look at impulse response functions 
 
@@ -200,7 +241,7 @@ causality(var.model, interest_rate)$Granger
 impulse = irf(
   var.model, 
   impulse = interest_rate,
-  response = "log_inflation_MOM",
+  response = inflation_measure,
   n.ahead = 25,
   ortho = FALSE,
   runs = 1000
@@ -218,23 +259,5 @@ impulse = irf(
 )
 plot(impulse)
 
-
-# Estimate structural var
-a = diag(1, 3)
-a[lower.tri(a)] = NA
-a
-svar = SVAR(var.model, Amat = a, max.iter = 10000,)
-svar
-
-impulse = irf(
-  svar, 
-  impulse = interest_rate,
-  response = "log_inflation_MOM",
-  n.ahead = 10,
-  ortho = TRUE,
-  runs = 1000
-)
-
-
-
-
+# We see that the IPR function has a similar shape as the coefficients make
+# For the distributed lag model above.
