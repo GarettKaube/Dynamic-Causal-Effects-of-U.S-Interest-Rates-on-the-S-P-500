e25a750e30c93445ada3c0ebb7b888893e485b2c
Garett Kaube <108091263+GarettKaube@users.noreply.github.com>
2024-08-27 09:43:20 -0600

    Update data.R

--- src/data.R
+++ src/data.R
@@ -13,6 +13,8 @@ library(tseries)
 library(dynlm)
 library(plotly)
 library(yaml)
+library(quantmod)
+library(lubridate)
 
 source("src/helpers.R")
 
@@ -20,16 +22,14 @@ code = readLines("./FRED/fred-api-key.txt")
 code
 fredr_set_key(code)
 
-
-
-get_data = function(hash_funct, start_=as.Date("2010-01-01")) {
+get_fred_data = function(hash_funct, start_=as.Date("1960-01-01")) {
+  # Gets the data in the hash_funct from FRED and merges them on date column
   series_ids = keys(hash_funct)
-  # Download time series 
+  # Download time series and store them in a list
   series_list = list()
   for (i in seq_along(series_ids)) {
     series = fredr(series_id=series_ids[i], observation_start=start_, frequency='m') %>% 
       drop_cols(name=hash_funct[[series_ids[i]]])
-    
     series_list = append(series_list, list(series))
   }
   
@@ -42,12 +42,44 @@ get_data = function(hash_funct, start_=as.Date("2010-01-01")) {
   return(merged)
 }
 
+get_yahoo_data = function(symbol, var_name) {
+  # Downloads S&P500 data from yahoo finance and 
+  # converts it to monthly data using the last value of 
+  # each month.
+  yh_data <- getSymbols(
+    symbol, 
+    src='yahoo', 
+    auto.assign=FALSE, 
+    from = '1960-01-01'
+  )
+  yh_data = apply.monthly(yh_data, last)
+  
+  # Convert to month start
+  index(yh_data) = floor_date(index(yh_data), "month")
+  yh_data = data.frame(yh_data)
+  
+  # Fix characters in the target variable
+  var = paste0(gsub("\\^", "", symbol), ".Close")
+  var = gsub("=", ".", var)
+  
+  # Reset index and rename the variable
+  yh_data = rownames_to_column(yh_data, "date") %>%
+    dplyr::rename_with(~var_name, var)
+  yh_data$date = as.Date(yh_data$date)
+
+  return(yh_data[, c("date", var_name)])
+}
+
+
+get_nom_gdp = function(start_=as.Date("1960-01-01")) {
+  series = fredr(series_id="GDP", observation_start=start_, frequency='q') %>% 
+    drop_cols(name="NOMINAL_GDP")
+  return(series)
+}
+
 
 data_pipeline = function() {
-  # Federal Funds Effective Rate, Bank of Canada Overnight Rate, Coinbase Bitcoin
-  # Hash function format is h[["FRED_ID"]] = "User choice name"
   h = hash()
-  
   
   # Structure of the data config yaml file is as follows: 
   # FRED_data:
@@ -62,65 +94,57 @@ data_pipeline = function() {
   data_config = yaml.load_file("./data_config/data_config.yaml")
   data_config_fred = data_config$FRED_data
   
+  # Hash function format is h[["FRED_ID"]] = "User choice name"
   for (data in names(data_config_fred)) {
     config = get(data, data_config_fred)
     if (get("download", config) == TRUE) {
       h[[data]] = get("name", config)
     }
   }
+  data = get_fred_data(h)
+  sp500 = get_yahoo_data("^GSPC", "SP500")
+  futures = get_yahoo_data('ZQ=F', "FEDFUNDS_FUTURES")
+  gdp = get_nom_gdp()
   
-
-  data = get_data(h)
+  data = merge(data, sp500, by="date")
+  data = merge(data, futures, by="date")
+  data = merge(data, gdp, by="date", all.x=TRUE)
+  data["NOMINAL_GDP"] = data["NOMINAL_GDP"] %>% fill(NOMINAL_GDP)
   
-  
-  # Create inflation variable
-  #data = data %>% mutate(inflation = (CPI/lag(CPI, n=12L) - 1))
+  # Create YOY log inflation variable
   data = difference_data(data, "CPI", lag = 12, log_diff = TRUE) %>% 
      dplyr::rename("log_inflation" = "CPI_DIFF")
-  
+  data$inflation = (data$CPI - lag(data$CPI, 12)) / lag(data$CPI, 12)
+  # MOM inflation
   data = difference_data(data, "CPI", lag = 1, log_diff = TRUE) %>% 
     dplyr::rename("log_inflation_MOM" = "CPI_DIFF")
   
-  data = difference_data(data, "producer_price_index", lag = 1, log_diff = TRUE) %>% 
-    dplyr::rename("producer_price_index_change" = "producer_price_index_DIFF")
-  
-  # create annual change in log inflation
-  #data = data %>% mutate(log_inflation_annual = log(CPI) - lag(log(CPI), n=12L))
-  
-  # Ex-post
-  data$REAL_INTEREST_3M = data$TREASURY3M - data$log_inflation
-  data$REAL_INTEREST_1Y = data$TREASURY1Y - data$log_inflation
-  #data$REAL_INTEREST_2Y = data$TREASURY2Y - data$inflation
-  data$REAL_INTEREST_10Y = data$TREASURY10Y - data$log_inflation
-  
 
-  
-  print(data)
   for (data_item in data_config_fred %>% names()) {
     config = get(data_item, data_config_fred)
     if (get("difference", config) == TRUE) {
       data = difference_data(data, get("name", config), log_diff = get("log", config))
     }
   }
-
   
   # Log transform TREASURY1Y
   data$log_TREASURY1Y = data$TREASURY1Y %>% log()
   
-  # Calculate average return of SP500
-  data = calculate_average_return("SP500", data)
-  data = lag_data(
-    data, 
-    c("TREASURY1Y_DIFF", "TREASURY3M_DIFF", 
-      "REAL_INTEREST_3M_DIFF", "FEDFUNDS_DIFF", 
-      "log_inflation", "CONSUMPTION_DIFF", "SAVINGS_DIFF",
-      "UNEMPLOYMENT_DIFF", "NONE_FARM_PAYROLLS", "producer_price_index_change"), 
-    lags=c(6,6,6,7,2, 1, 1, 2, 1, 1)
-  )
-
- 
+  data$FEDFUNDS_DIFF = (data$FEDFUNDS - lag(data$FEDFUNDS))/lag(data$FEDFUNDS)
+  
+  # Using method from Bernanke and Kuttner, 2005 to derive anticipated fed rate changes
+  data$FEDFUNDS_FUTURES_DIFF = (data$FEDFUNDS_FUTURES - lag(data$FEDFUNDS_FUTURES))/lag(data$FEDFUNDS_FUTURES)
+  data$unanticipated_rate = (100 - data$FEDFUNDS_FUTURES) - (100 - lag(data$FEDFUNDS_FUTURES))
+  data$anticipated_rate = c(NA, diff(data$FEDFUNDS)) - data$unanticipated_rate
+  data$anticipated = data$anticipated_rate <= 0.0025 & data$anticipated_rate >= -0.0025
+  
+  # Create date variables
+  data[, 'year'] = data[, 'date'] %>% sapply(extract_year)
+  data[, 'month'] = data[, 'date'] %>% sapply(extract_month)
   
   # Remove NA
+  data$FEDFUNDS_DIFF_DIFF = c(NA, base::diff(data$FEDFUNDS_DIFF))
+  data$log_inflation_DIFF = c(NA, base::diff(data$log_inflation))
   data = na.omit(data)
   return(data)
 }
