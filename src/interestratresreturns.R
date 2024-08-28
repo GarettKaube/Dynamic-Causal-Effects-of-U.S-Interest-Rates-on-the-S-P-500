0d4ce87387db89c3651543c7b36d7e0bae773472
Garett Kaube <108091263+GarettKaube@users.noreply.github.com>
2024-08-28 17:46:13 -0600

    Update interestratresreturns.R

--- src/interestratresreturns.R
+++ src/interestratresreturns.R
@@ -13,71 +13,34 @@ library(tseries)
 library(dynlm)
 library(plotly)
 library(MASS)
-library(robustbase) 
-library(olsrr)
 library(readtext)
+library(vars)
 library(ggcorrplot)
 
 source("src/helpers.R")
 source("src/data.R")
 
-
 code = readLines("./FRED/fred-api-key.txt")
 fredr_set_key(code)
 
+# Data prep
 data = data_pipeline()
 row.names(data) = 1:dim(data)[1]
 dim(data)
 tail(data)
 head(data,1)
 data$anticipated = as.numeric(data$anticipated)
+# Covid dummy variable
+data$COVID = sapply(data$date, covid_dummy)
 # Note: SP500_DIFF is in log differences, i.e log returns
 
 data.ts = ts(dplyr::select(data, -c("date")), start = c(2002, data[1, "month"]), freq=12)
-data.ts = window(data.ts, end = c(2019,1))
 dim(data.ts)
 
 
+# Look at cross correlation
+print(ccf(data$FEDFUNDS_DIFF, data$log_inflation, lag.max= 20))
 
-# Look at cross correlations
-print(ccf(data$TREASURY1Y_DIFF, data$log_inflation, lag.max= 20))
-print(ccf(data$TREASURY3M_DIFF, data$log_inflation), lag.max= 20)
-print(ccf(data$TREASURY3M_DIFF, data$SP500_DIFF, lag.max= 20))
-
-
-
-# Assess multicollinearity 
-get_VIF = function(predictors) {
-  # Initialize VIF data frame
-  VIF = data.frame()
-  for (i in 1:(length(predictors))) {
-    target = predictors[i]
-    
-    # Get predictors
-    predictors_modified = predictors[-c(i)]
-    
-    # Create regression formulas
-    predictor_str = predictors_modified[1]
-    predictors_modified = predictors_modified[-c(1)]
-    for (pred in predictors_modified) {
-      predictor_str = predictor_str %>% paste("+") %>% paste(pred)
-    }
-    
-    formula = paste(target, "~") %>% paste(predictor_str) 
-    print(paste("Model: ",formula))
-    
-    # Regress, get R-Squared and calculate Variance factors
-    r2 = summary(lm({{formula}}, data = data))$r.squared
-    VIF[1, paste(formula, "|")] =  1/(1-r2)
-    
-  }
-  return(VIF)
-}
-
-predictors = c("CONSUMPTION_DIFF", "log_inflation", "SAVINGS_DIFF", "FEDFUNDS_DIFF"
-               ,"UNEMPLOYMENT_DIFF", "NONE_FARM_PAYROLLS", "VOLATILITY_INDEX_DIFF",
-               "FEDFUNDS_FUTURES_DIFF", "NOMINAL_GDP_DIFF")
-get_VIF(predictors)
 
 # Look at correlation between the variables
 cor(data.ts[,c("UNEMPLOYMENT_DIFF", "CONSUMPTION_DIFF")])
@@ -113,19 +76,19 @@ coeftest(model, vcov. = nw)
 
 # Distributed lag model with no controls
 model = dynlm(
-  SP500_DIFF ~  d(L(FEDFUNDS_DIFF, 0:4)) + L(FEDFUNDS_DIFF, 5), 
+  SP500_DIFF ~  L(d(FEDFUNDS_DIFF), 0:4) + L(FEDFUNDS_DIFF, 5), 
   data=data.ts
 )
-# HAC standard errors for testing model coefficients
+
 m = ceiling(0.75 * (dim(data.ts)[1])**(1/3))
 nw = NeweyWest(model, lag = m, prewhite = F)
 coeftest(model, vcov. = nw)
 
 #------ Dynamic Linear Model---------
 # Controls:
-# - Present and past consumption growth, inflation, and savings growth: 
-#    Present values will directly effect firms and consumers 
-#    and investors react to the months previous values.
+# - Surprise in consumption growth, inflation, and unemployment: 
+#    Expected values will already be incorporated in the S&P500 returns
+#    unless there is an unexpected change.
 # - Volatility index:
 #    Captures investors market sentiment.
 # - Anticipated rate changes (anticipated):
@@ -135,16 +98,73 @@ coeftest(model, vcov. = nw)
 # effect future federal funds rates through increased consumption.
 # but SP500 may respond to future forecasts of inflation and a change in FED EFFECTIVE RATE.
 
+# We use a VAR's residuals as a measure of surprise:
+
+get_surprise_vars = function(data.ts) {
+  TYPE = 'const'
+  variables = c("log_inflation", 
+                "CONSUMPTION_DIFF", 
+                "UNEMPLOYMENT_DIFF", 
+                "SAVINGS_DIFF",
+                "TREASURY10Y_DIFF")
+  var_data = data.ts[, variables]
+  
+  # Select lag that minimizes HQ (Hannan–Quinn information criterion)
+  selection = VARselect(
+    var_data, 
+    lag.max=6, 
+    type=TYPE
+  )[["selection"]]
+  
+  var.model = VAR(var_data, p=selection[2], type=TYPE)
+
+  # Join the residuals to the original data
+  surprise = residuals(var.model)
+  new_col_names = c()
+  for (i in 1:length(variables)) {
+    new_col_names[i] = paste0(variables[i], "_surprise")
+  }
+  colnames(surprise) = new_col_names
+  combined = cbind(
+    as.data.frame(data.ts), 
+    rbind(
+      matrix(rep(NA, 5*selection[2]), nrow=selection[2], ncol = 5), 
+      surprise
+    )
+  )
+  combined  = na.omit(combined )
+  combined.ts = ts(combined , start = c(2002, combined[1, "month"]), freq=12)
+  return(combined.ts)
+}
+
+combined= get_surprise_vars(data.ts)
+
+# Replot correlations
+vars = c(vars, new_col_names)
+corr = cor(combined.ts[, vars])
+ggcorrplot(corr, hc.order = TRUE, outline.col = "white")
+
+cor(combined.ts[, c("SAVINGS_DIFF_surprise", 
+                    "UNEMPLOYMENT_DIFF_surprise", 
+                    "CONSUMPTION_DIFF_surprise")])
+
+# Check VIF
+predictors = c("SAVINGS_DIFF_surprise", "UNEMPLOYMENT_DIFF_surprise", 
+               "CONSUMPTION_DIFF_surprise", "log_inflation_surprise", "FEDFUNDS_DIFF", 
+               "VOLATILITY_INDEX_DIFF")
+
+get_VIF(predictors, combined.ts)
+
 model = dynlm(
-  SP500_DIFF ~  L(FEDFUNDS_DIFF, 0:10) + 
-    L(log_inflation) + log_inflation + VOLATILITY_INDEX_DIFF + 
-    anticipated + L(CONSUMPTION_DIFF, 0:1) + L(SAVINGS_DIFF, 0:1), 
-  data=data.ts
+  SP500_DIFF ~  L(FEDFUNDS_DIFF, 0:10) +
+    log_inflation_surprise + CONSUMPTION_DIFF_surprise + 
+    VOLATILITY_INDEX_DIFF + COVID + anticipated + UNEMPLOYMENT_DIFF_surprise, 
+  data=combined.ts
 )
 summary(model)
 
 # HAC standard errors
-m = ceiling(0.75 * (dim(data.ts)[1])**(1/3))
+m = ceiling(0.75 * (dim(combined.ts)[1])**(1/3))
 nw = NeweyWest(model, lag = m, prewhite = F)
 coeftest(model, vcov. = nw)
 
@@ -158,26 +178,23 @@ ggplot(data = coef_df, aes(x=1:(lags+1), y=coef.model..2..lags...2.., group=1)) 
   theme(axis.text.x = element_text(angle = 65, hjust = 1)) +
   ylab("Coefficient") +
   xlab("Lag") + 
-  ggtitle("Cumulative Dynamic Effect of Fed Effective Rate on S&P500") +
+  ggtitle("Dynamic Effect of Fed Effective Rate on S&P500") +
   theme_minimal()
 
-# We see that a increase in the FED EFFECTIVE Rate causes the SP500 to decline
-# and quickly recovers
 
 # Look at autocorrelation and partial autocorrelation
 ggAcf(residuals(model))
 ggPacf(residuals(model))
 
 # Assess residuals
-residual_plot("FEDFUNDS_DIFF", model, data=data.ts,lags=lags)
+residual_plot("FEDFUNDS_DIFF", model, data=combined.ts,lags=lags)
 ggplotly()
-residual_plot("log_inflation", model, data=data.ts,lags=lags)
-residual_plot("CONSUMPTION_DIFF", model, data=data.ts,lags=lags)
-residual_plot("SAVINGS_DIFF", model, data=data.ts,lags=lags)
-residual_plot("VOLATILITY_INDEX_DIFF", model, data=data.ts,lags=lags)
+residual_plot("log_inflation_surprise", model, data=combined.ts, lags=lags)
+residual_plot("CONSUMPTION_DIFF_surprise", model, data=combined.ts, lags=lags)
+residual_plot("SAVINGS_DIFF_surprise", model, data=combined.ts, lags=lags)
+residual_plot("VOLATILITY_INDEX_DIFF", model, data=combined.ts,lags=lags)
 
 # F-test with HAC errors
-
 ANOVA_DATA = data.ts[(lags+1) : dim(data.ts)[1]-1, ]
 row.names(ANOVA_DATA) = 1:dim(ANOVA_DATA)[1]
 ANOVA_DATA = ts(ANOVA_DATA)
@@ -196,7 +213,6 @@ waldtest(model,
 # and therefore in the next period, the returns on the stocks/bonds
 # can effect consumer demand then consumer demand effects inflation and hence indirectly
 # the Fed Funds Rate.
-library(vars)
 
 interest_rate = "FEDFUNDS_DIFF"
 inflation_measure = "log_inflation"
@@ -207,7 +223,9 @@ TYPE = 'const'
 var_data = data.ts[, c(inflation_measure, 
                        "CONSUMPTION_DIFF", 
                        "UNEMPLOYMENT_DIFF", 
-                       interest_rate, "SP500_DIFF")
+                       interest_rate, 
+                       "SAVINGS_DIFF",
+                       "SP500_DIFF")
                     ]
 print(ccf(var_data[, inflation_measure], var_data[, interest_rate], lag.max=10))
 
